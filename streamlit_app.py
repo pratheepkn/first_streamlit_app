5081f21dd8c08221fc73561f5a047bac6e36948e
pratheepkn <31509178+pratheepkn@users.noreply.github.com>
2023-08-26 01:00:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,18 +39,34 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.dataframe(fruityvice_normalized)
 #*/
 
-streamlit.header('Fruityvice Fruit Advice!')
+#/*
+#streamlit.header('Fruityvice Fruit Advice!')
+#try:
+ # fruit_choice = streamlit.text_input('What fruit would like to information about?')
+  #if not fruit_choice:
+   # streamlit.error("Please select a fruit to get information.")
+  #else:
+   # fruityvise_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #streamlit.dataframe(fruityvice_normalized)
+#except URLError as e:
+ # streamlit.error()
+#*/
+#create the repeatable code block called function
+def get_fruityvice_data(this_fruit_choice):
+    fruityvise_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+#New section to display fruityvice api response
+streamlit.header('Fruityvice fruit advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would like to information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvise_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
-
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
 
 #Connect with snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
